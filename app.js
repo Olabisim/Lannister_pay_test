65dc40fd2b2f3edddc01c8da24616a9df9bb209d
holabisii <ajoseholabisi@gmail.com>
2022-07-20 23:39:54 -0400

    cors added

--- app.js
+++ app.js
@@ -6,9 +6,36 @@ import { split_payment } from './controllers/index.js'
 
 import express from 'express'
 import responseTime from 'response-time';
+import cors from 'cors';
 
 
 const app = express();
+
+
+// CORS PROBLEM ALLOWING FRONTEND TO PROCESS HTTP METHODS
+
+app.use(cors());
+
+app.use(function (req, res, next) {
+        // Website you wish to allow to connect
+        res.setHeader("Access-Control-Allow-Origin", "*");
+
+        // Request methods you wish to allow
+        res.setHeader(
+                "Access-Control-Allow-Methods",
+                "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+        );
+
+        // Request headers you wish to allow
+        res.setHeader(
+                "Access-Control-Allow-Headers",
+                "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization"
+        );
+        next();
+});
+
+// CORS PROBLEM END
+
 
 app.use(express.json());
 app.use(responseTime((req, res, time) => {
