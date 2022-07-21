7d58bd480d06b323b521beb8f3f548afedcf526a
holabisii <ajoseholabisi@gmail.com>
2022-07-20 23:29:42 -0400

    added response time

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ const __dirname = dirname(__filename);
 import { split_payment } from './controllers/index.js'
 
 import express from 'express'
+import responseTime from 'response-time';
 
 
 const app = express();
 
 app.use(express.json());
+app.use(responseTime((req, res, time) => {
+        console.log(`${req.method} ${req.url} ${time}ms`);
+}))
 
 app.post('/split-payments/compute', split_payment)
 
